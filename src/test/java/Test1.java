cc8827b0ab9c959350bf20698eedfd520a2bcdda
wyc <HitWyc1994@163.com>
2022-08-08 19:10:37 +0800

    《Java语言程序设计》测试了String和整型以及浮点型的强转

---
+++ src/test/java/Test1.java
@@ -0,0 +1,21 @@
+/**
+ * @author wangyc
+ * @version 1.11
+ * @description 为了测试一下强制类型转换
+ * @data 2022/8/8 19:01
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        String a = "13";
+        int b = Integer.parseInt(a);
+        String c = String.valueOf(b);
+        String d = "12.5";
+        double e = Double.parseDouble(d);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
