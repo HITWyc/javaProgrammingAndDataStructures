08551519525522fd9fdfa1506904843afb99546a
wyc <HitWyc1994@163.com>
2022-08-08 15:44:33 +0800

    《Java语言程序设计》开启

---
+++ src/main/java/chart2/section13/ShowCurrentTime.java
@@ -0,0 +1,38 @@
+package chart2.section13;
+
+/**
+ * @author wangyc
+ * @version 1.11
+ * @description 通过调用System.currentTimeMillis()返回当前时间。
+ * @data 2022/8/8 14:26
+ */
+public class ShowCurrentTime {
+    public static void main(String[] args) {
+        long currentTimeMillis = System.currentTimeMillis();
+
+        // Obtain the total milliseconds since midnight, Jan 1, 1970
+        long totalMilliseconds = System.currentTimeMillis();
+
+        // Obtain the total seconds since midnight, Jan 1, 1970
+        long totalSeconds = totalMilliseconds / 1000;
+
+        // Compute the current second in the minute in the hour
+        long currentSecond = totalSeconds % 60;
+
+        // Obtain the total minutes
+        long totalMinutes = totalSeconds / 60;
+
+        // Compute the current minute in the hour
+        long currentMinute = totalMinutes % 60;
+
+        // Obtain the total hours
+        long totalHours = totalMinutes /  60;
+
+        // Computer the current hour
+        long currentHour = totalHours % 24;
+
+        // Display results
+        System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond + "GMT");
+        System.out.println("消耗的时间为" + (System.currentTimeMillis() - currentTimeMillis));
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * @author wangyc
+ * @version 1.11
+ * @description
+ * @data ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
